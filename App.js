9394420206ceb6805185c63dad21141fbe992cff
swaneet <75112641+swaneet@users.noreply.github.com>
2024-02-26 20:16:35 +0530

    Episode-2

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
-<div id="root">
-    <div id="parent">
-        <div id="child">
-            <h1 id="heading1">I M heading1</h1>
-            <h2 id="heading2">I M heading2</h2>
-        </div>
-        <div id="child2">
-            <h1 id="heading1">I M heading1</h1>
-            <h2 id="heading2">I M heading2</h2>
-        </div>
-    </div>
-</div>
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child" },
-    [React.createElement("h1", { id: "heading1" }, "I M heading1"),
+    [React.createElement("h1", { id: "heading1" }, "I M heading1🔥"),
     React.createElement("h2", { id: "heading2" }, "I M heading2")]
 ),
 React.createElement("div", { id: "child2" },
@@ -23,25 +12,6 @@ React.createElement("div", { id: "child2" },
 );
 
 
-
-
-
-
-
-
  const root = ReactDOM.createRoot(document.getElementById("root"));
 
  root.render(parent);
-
-
-
-// const heading = React.createElement("h1",
-//     {id:"heading"},
-//     "Hello World From React");
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-
-
